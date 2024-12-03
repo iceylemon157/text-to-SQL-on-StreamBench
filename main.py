3b7700f9b0d171f918bdd176fac7d08240914126
iceylemon157 <oosheepyerd79135@gmail.com>
2024-12-03 19:29:04 +0800

    test: use bm25 for RAG

--- main.py
+++ main.py
@@ -1,56 +1,197 @@
 from base import Agent
 from execution_pipeline import main
 
-class ClassificationAgent(Agent):
+import re
+import random
+from colorama import Fore, Style
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+from utils import RAG, strip_all_lines
+
+class LocalModelAgent(Agent):
     """
-    An agent that classifies text into one of the labels in the given label set.
+    A base agent that uses a local model for text generation tasks.
     """
     def __init__(self, config: dict) -> None:
         """
-        Initialize your LLM here
+        Initialize the local model
         """
-        # TODO
-        raise NotImplementedError
+        super().__init__(config)
+        self.llm_config = config
+        if config['use_8bit']:
+            quantization_config = BitsAndBytesConfig(
+                load_in_8bit=True,
+                llm_int8_has_fp16_weight=False
+            )
+            self.model = AutoModelForCausalLM.from_pretrained(
+                config["model_name"],
+                quantization_config=quantization_config,
+                device_map=config["device"]
+            )
+        else:
+            self.model = AutoModelForCausalLM.from_pretrained(
+                config["model_name"],
+                torch_dtype=torch.float16,
+                device_map=config["device"]
+            )
+        self.tokenizer = AutoTokenizer.from_pretrained(config["model_name"])
+        self.rag = RAG(config["rag"])
+        # Save the streaming inputs and outputs for iterative improvement
+        self.inputs = list()
+        self.self_outputs = list()
+        self.model.eval()
+
+    def generate_response(self, messages: list) -> str:
+        """
+        Generate a response using the local model.
+        """
+        text_chat = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = self.tokenizer([text_chat], return_tensors="pt").to(self.model.device)
+
+        generated_ids = self.model.generate(
+            **model_inputs,
+            max_new_tokens=self.llm_config["max_tokens"],
+            do_sample=False
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        return self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    def update(self, correctness: bool) -> bool:
+        """
+        Update the agent based on the correctness of its output.
+        """
+        if correctness:
+            question = self.inputs[-1]
+            answer = self.self_outputs[-1]
+            chunk = self.get_shot_template().format(question=question, answer=answer)
+            self.rag.insert(key=question, value=chunk)
+            return True
+        return False
+
+class ClassificationAgent(LocalModelAgent):
+    """
+    An agent that classifies text into one of the labels in the given label set.
+    """
+    @staticmethod
+    def get_system_prompt() -> str:
+        system_prompt = """\
+        Act as a professional medical doctor that can diagnose the patient based on the patient profile.
+        Provide your diagnosis in the following format: <number>. <diagnosis>""".strip()
+        return strip_all_lines(system_prompt)
+
+    @staticmethod
+    def get_zeroshot_prompt(
+        option_text: str,
+        text: str
+    ) -> str:
+        prompt = f"""\
+        Act as a medical doctor and diagnose the patient based on the following patient profile:
+
+        {text}
+
+        All possible diagnoses for you to choose from are as follows (one diagnosis per line, in the format of <number>. <diagnosis>):
+        {option_text}
+
+        Now, directly provide the diagnosis for the patient in the following format: <number>. <diagnosis>""".strip()
+        return strip_all_lines(prompt)
+
+    @staticmethod
+    def get_shot_template() -> str:
+        prompt = f"""\
+        {{question}}
+        Diagnosis: {{answer}}"""
+        return strip_all_lines(prompt)
+
+    @staticmethod
+    def get_fewshot_template(
+        option_text: str,
+        text: str,
+    ) -> str:
+        prompt = f"""\
+        Act as a medical doctor and diagnose the patient based on the provided patient profile.
+        
+        All possible diagnoses for you to choose from are as follows (one diagnosis per line, in the format of <number>. <diagnosis>):
+        {option_text}
+
+        Here are some example cases.
+        
+        {{fewshot_text}}
+        
+        Now it's your turn.
+        
+        {text}        
+        
+        Now provide the diagnosis for the patient in the following format: <number>. <diagnosis>"""
+        return strip_all_lines(prompt)
 
     def __call__(
         self,
         label2desc: dict[str, str],
         text: str
     ) -> str:
-        """
-        Classify the text into one of the labels.
+        self.reset_log_info()
+        option_text = '\n'.join([f"{str(k)}. {v}" for k, v in label2desc.items()])
+        system_prompt = self.get_system_prompt()
+        prompt_zeroshot = self.get_zeroshot_prompt(option_text, text)
+        prompt_fewshot = self.get_fewshot_template(option_text, text)
+        
+        shots = self.rag.retrieve(query=text, top_k=self.rag.top_k) if (self.rag.insert_acc > 0) else []
+        if len(shots):
+            fewshot_text = "\n\n\n".join(shots).replace("\\", "\\\\")
+            try:
+                prompt = re.sub(pattern=r"\{fewshot_text\}", repl=fewshot_text, string=prompt_fewshot)
+            except Exception as e:
+                error_msg = f"Error ```{e}``` caused by these shots. Using the zero-shot prompt."
+                print(Fore.RED + error_msg + Fore.RESET)
+                prompt = prompt_zeroshot
+        else:
+            print(Fore.YELLOW + "No RAG shots found. Using zeroshot prompt." + Fore.RESET)
+            prompt = prompt_zeroshot
+        
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+        ]
+        response = self.generate_response(messages)
+        prediction = self.extract_label(response, label2desc)
+        
+        self.update_log_info(log_data={
+            "num_input_tokens": len(self.tokenizer.encode(system_prompt + prompt)),
+            "num_output_tokens": len(self.tokenizer.encode(response)),
+            "num_shots": str(len(shots)),
+            "input_pred": prompt,
+            "output_pred": response,
+        })
+        self.inputs.append(text)
+        self.self_outputs.append(f"{str(prediction)}. {label2desc[int(prediction)]}")
+        return prediction
 
-        Args:
-            label2desc (dict[str, str]): A dictionary mapping each label to its description.
-            text (str): The text to classify.
-
-        Returns:
-            str: The label (should be a key in label2desc) that the text is classified into.
-
-        For example:
-        label2desc = {
-            "apple": "A fruit that is typically red, green, or yellow.",
-            "banana": "A long curved fruit that grows in clusters and has soft pulpy flesh and yellow skin when ripe.",
-            "cherry": "A small, round stone fruit that is typically bright or dark red.",
-        }
-        text = "The fruit is red and about the size of a tennis ball."
-        label = "apple" (should be a key in label2desc, i.e., ["apple", "banana", "cherry"])
-        """
-        # TODO
-        raise NotImplementedError
-
-    def update(self, correctness: bool) -> bool:
-        """
-        Update your LLM agent based on the correctness of its own prediction at the current time step.
-
-        Args:
-            correctness (bool): Whether the prediction is correct.
-
-        Returns:
-            bool: Whether the prediction is correct.
-        """
-        # TODO
-        raise NotImplementedError
+    @staticmethod
+    def extract_label(pred_text: str, label2desc: dict[str, str]) -> str:
+        numbers = re.findall(pattern=r"(\d+)\.", string=pred_text)
+        if len(numbers) == 1:
+            number = numbers[0]
+            if int(number) in label2desc:
+                prediction = number
+            else:
+                print(Fore.RED + f"Prediction {pred_text} not found in the label set. Randomly select one." + Style.RESET_ALL)
+                prediction = random.choice(list(label2desc.keys()))
+        else:
+            if len(numbers) > 1:
+                print(Fore.YELLOW + f"Extracted numbers {numbers} is not exactly one. Select the first one." + Style.RESET_ALL)
+                prediction = numbers[0]
+            else:
+                print(Fore.RED + f"Prediction {pred_text} has no extracted numbers. Randomly select one." + Style.RESET_ALL)
+                prediction = random.choice(list(label2desc.keys()))
+        return str(prediction)
 
 class SQLGenerationAgent(Agent):
     """
@@ -94,11 +235,19 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument('--bench_name', type=str, required=True)
+    parser.add_argument('--model_name', type=str, default="Qwen/Qwen2.5-7B-Instruct")
+    parser.add_argument('--device', type=str, default="cuda:0")
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--use_8bit', action='store_true')
+    parser.add_argument('--output_path', type=str, default=None, help='path to save csv file for kaggle submission')
+    parser.add_argument('--use_wandb', action='store_true')
     args = parser.parse_args()
 
     if args.bench_name.startswith("classification"):
+        max_tokens = 16
         agent_name = ClassificationAgent
     elif args.bench_name.startswith("sql_generation"):
+        max_tokens = 512
         agent_name = SQLGenerationAgent
     else:
         raise ValueError(f"Invalid benchmark name: {args.bench_name}")
@@ -108,6 +257,19 @@ if __name__ == "__main__":
     }
     config = {
         # TODO: specify your configs for the agent here
+        'model_name': args.model_name,
+        'exp_name': f'self_streamicl_{args.bench_name}_{args.model_name}',
+        'bench_name': bench_cfg['bench_name'],
+        'max_tokens': max_tokens,
+        'do_sample': False,
+        'device': args.device,
+        'use_8bit': args.use_8bit,
+        'rag': {
+            'embedding_model': 'BAAI/bge-base-en-v1.5',
+            'seed': 42,
+            "top_k": 16,
+            "order": "similar_at_top"
+        }
     }
     agent = agent_name(config)
     main(agent, bench_cfg)
