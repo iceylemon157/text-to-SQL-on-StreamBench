fe47887a06f0d86244b3ce87d1c9a81d281c1ec0
iceylemon157 <oosheepyerd79135@gmail.com>
2025-04-21 02:40:17 +0800

    refactor: update README and run.sh
    1. Rename the course README
    2. Replace course README with repo README
    3. update run.sh for this repo

--- run.sh
+++ run.sh
@@ -1,20 +1,12 @@
 bench_names=(
-    "classification_public"
-    "classification_private"
     "sql_generation_public"
     "sql_generation_private"
 )
-model_name="prince-canuma/Ministral-8B-Instruct-2410-HF"
-device="cuda:1"
+model_name="meta-llama/Llama-3.1-8B-Instruct"
+device="auto"
 
-echo -e "\033[0;36mRunning Zero-Shot:\033[0m"
+echo -e "\033[0;36mRunning Lora StreamICL for SQL Generation:\033[0m"
 for bench_name in ${bench_names[@]}; do
     echo -e "\033[0;32mRunning benchmark: $bench_name\033[0m"
-    python -m examples.zeroshot --bench_name $bench_name --model_name $model_name --device $device
-done
-
-echo -e "\033[0;36mRunning Self-StreamICL:\033[0m"
-for bench_name in ${bench_names[@]}; do
-    echo -e "\033[0;32mRunning benchmark: $bench_name\033[0m"
-    python -m examples.self_streamicl --bench_name $bench_name --model_name $model_name --device $device
+    python lora_streamicl-sql.py --bench_name $bench_name --model_name $model_name
 done
